36d5c6c6181f0ea33ef07de6f25ff9e02a2d0aff
Kelsey Gilmore-Innis <kelsey@helloreverb.com>
2013-12-03 22:03:38 -0800

    handle function values

--- core/src/main/scala/Test.scala
+++ core/src/main/scala/Test.scala
@@ -1,36 +1,96 @@
+package com.kelseyinnis.detonator
+
 object Test extends App {
   val result = "hello world"
-  val test: String = TheObject.sing("hi")
+  val test: String = PrimitiveObject.sing("hi")
   println(test.asInstanceOf[AnyRef].getClass.getSimpleName)
   println(test)
   println(test)
   Thread.sleep(1000)
   println(test)
   Thread.sleep(1000)
-  val test2: String = TheObject.sing("hi3")
+  val test2: String = PrimitiveObject.sing("hi3")
   println(test2.asInstanceOf[AnyRef].getClass.getSimpleName)
   println(test2)
   Thread.sleep(1000)
   println(test2)
   println(test2)
-  def intTest: Long = TheObject.intSing("hi4")
+  def intTest: Long = PrimitiveObject.intSing("hi4")
   println(intTest.asInstanceOf[AnyRef].getClass.getSimpleName)
   println(intTest)
   println(intTest)
   println(intTest)
   Thread.sleep(1000)
   println(intTest)
-  val doubleTest: String = TheObject.doubleSing("hi5", 7)
+  val doubleTest: String = PrimitiveObject.doubleSing("hi5", 7)
   println(doubleTest.asInstanceOf[AnyRef].getClass.getSimpleName)
   println(doubleTest)
   println(doubleTest)
   println(doubleTest)
   Thread.sleep(1000)
   println(doubleTest)
+  val listTest: List[Int] = PrimitiveObject.listSing(Some(365))
+  println(listTest.asInstanceOf[AnyRef].getClass.getSimpleName)
+  println(listTest)
+  println(listTest)
+
+  val ccTest = ComplexObject.caseClassParam(Banana("hi", 5))
+  println(ccTest.asInstanceOf[AnyRef].getClass.getSimpleName)
+  println(ccTest)
+  println(ccTest)
+
+  val caseClassReturned = ComplexObject.caseClassReturned("apple")
+  println(caseClassReturned.asInstanceOf[AnyRef].getClass.getSimpleName)
+  println(caseClassReturned)
+  println(caseClassReturned)
+
+  val functionTest = ComplexObject.functionDef(100)
+  println(functionTest.asInstanceOf[AnyRef].getClass.getSimpleName)
+  println(functionTest)
+  println(functionTest)
+
+  val valAsFunctionTest = ComplexObject.functionVal(List(1,2,3))
+  println(valAsFunctionTest.asInstanceOf[AnyRef].getClass.getSimpleName)
+  println(valAsFunctionTest)
+  println(valAsFunctionTest)
+
+  val curriedTest = ComplexObject.curried("this is a string")
+  println(curriedTest.asInstanceOf[AnyRef].getClass.getSimpleName)
+  println(curriedTest)
+  println(curriedTest)
 }
 
-object TheObject {
+object PrimitiveObject {
   val sing = Macros.singleton{ (test: String) => { val time = System.currentTimeMillis().toString; println(time); time } }
-  val intSing = Macros.singleton { (test: String) => { val time = System.currentTimeMillis(); println(test + " " + time); time } }
+  lazy val intSing = Macros.singleton { (test: String) => { val time = System.currentTimeMillis(); println(test + " " + time); time } }
   val doubleSing = Macros.singleton{ (test1: String, test2: Int) => { val time = System.currentTimeMillis().toString; println(test1 + " " + time + " " + test2); time } }
+  val listSing = Macros.singleton { (testParam: Option[Int]) => println("listTest " + System.currentTimeMillis()); testParam.map(v => List(v, v, v)).getOrElse(List(0,0,0))}
 }
+
+object ComplexObject {
+
+  val caseClassParam = Macros.singleton{ (ccParam: Banana) => { println(System.currentTimeMillis().toString); ccParam.toString } }
+  val caseClassReturned = Macros.singleton{ (test: String) => { println(System.currentTimeMillis().toString); Banana(test, 200) } }
+
+  def newFunction(something: Int): String = (something * 2).toString
+  val functionDef = Macros.singleton {newFunction _}
+
+  def curryable(oneParam: Banana, twoParam: String) = oneParam.test2 + twoParam.length
+  val curried = Macros.singleton(curryable(Banana("hola", 1000), _: String))
+
+  val valFunction = (something: List[Int]) => (something.map(_ * 3)).toString + System.currentTimeMillis()
+  val functionVal = Macros.singleton(valFunction)
+
+//  //TODO: should fail
+//  val wrappedString = "hello"
+//  val stringAsFunction = Macros.singleton(wrappedString)
+//
+//  //TODO: should fail
+//  val nonPrimitive = Banana("yo", 10)
+//  val nonFunction = Macros.singleton(nonPrimitive)
+
+  val fakeFunction = System.currentTimeMillis()
+  val valAsFunction = Macros.singleton {fakeFunction _}
+}
+
+case class Banana(test: String, test2: Int)
--- macros/src/main/scala/Macros.scala
+++ macros/src/main/scala/Macros.scala
@@ -1,11 +1,13 @@
+package com.kelseyinnis.detonator
+
 import language.experimental.macros
 import scala.reflect.macros.Context
 
 object Macros {
 
-  def singleton[R](param: Any) = macro singletonImpl[R]
+  def singleton[T](param: T) = macro singletonImpl[T]
 
-  def singletonImpl[R](c: Context)(param: c.Expr[Any]) = {
+  def singletonImpl[T](c: Context)(param: c.Expr[T])(implicit tt: c.WeakTypeTag[T]) = {
     import c.universe._
     import nme.CONSTRUCTOR
     import tpnme.EMPTY
@@ -14,62 +16,83 @@ object Macros {
     val constructor = DefDef(Modifiers(), CONSTRUCTOR, List(), List(List()), TypeTree(), constructorBody)
     val objInheritance = List(Ident(newTypeName("AnyRef")))
 
+    def convertFunctionToSingletonObject(func: Function, returnType: Option[Type] = None) = {
+
+      val body = func.body
+      val params = func.vparams
+
+      val inst = ValDef(Modifiers(Flag.MUTABLE), newTermName("_inst"), TypeTree(appliedType(
+        typeOf[Option[_]].typeConstructor, List(returnType getOrElse body.tpe))), reify {
+        None
+      }.tree)
+
+      val declareParams = params.map { p =>
+        ValDef(Modifiers(Flag.PARAM), p.name, p.tpt, EmptyTree)
+      }
+
+      val declareInst = DefDef(Modifiers(), newTermName("_declareInst"), List(),
+        List(declareParams),
+        TypeTree(),
+        Block(List(
+          ValDef(Modifiers(), newTermName("_res"), TypeTree(), c.resetAllAttrs(body)),
+          Assign(Ident(newTermName("_inst")),
+            Apply(Select(Select(Ident("scala"), newTermName("Some")), newTermName("apply")),
+              List(Ident(newTermName("_res"))))
+          )), Ident(newTermName("_res"))))
+
+
+      val applyDef = DefDef(Modifiers(), newTermName("apply"), List(),
+        List(params),
+        TypeTree(), Apply(Select(Ident(newTermName("_inst")), newTermName("getOrElse")),
+          List(Apply(Ident(newTermName("_declareInst")), params.map {
+            p => Ident(p.name)
+          }))))
+
+      val objectName = newTermName(c.fresh("TheObject"))
+
+      val obj = ModuleDef(Modifiers(), objectName, Template(objInheritance, emptyValDef, List(constructor, inst, declareInst, applyDef)))
+
+      val applyParams =  params.map { p =>
+        ValDef(Modifiers(Flag.PARAM), c.fresh(p.name), p.tpt, EmptyTree)
+      }
+
+      val partiallyApplied = Function(
+        applyParams,
+        Apply(Select(Ident(objectName), newTermName("apply")), applyParams.map {
+          p => Ident(p.name)
+        }))
+
+
+      val block = Block(List(obj), partiallyApplied)
+
+     // println(show(block))
+
+      c.Expr[T](block)
+    }
+//    println(param.tree)
+//    println(param.actualType)
+//    println("T: " + tt.tpe)
+    //   println("U: " + ut.tpe)
+
+    val funcs = (0 to 22).map { c.universe.definitions.FunctionClass(_) }.toList
+
     //TODO: verify return type, throw error if no match
     param.tree match {
-      case Function(params, body) => {
-        println("it's a function with params: " + params)
-
-        val inst = ValDef(Modifiers(Flag.MUTABLE), newTermName("_inst"), TypeTree(appliedType(
-          typeOf[Option[_]].typeConstructor, List(body.tpe))), reify {
-          None
-        }.tree)
-
-        val declareParams = params.map { p =>
-          ValDef(Modifiers(Flag.PARAM), p.name, p.tpt, EmptyTree)
-        }
-
-        val declareInst = DefDef(Modifiers(), newTermName("_declareInst"), List(),
-          List(declareParams),
-          TypeTree(),
-          Block(List(
-            ValDef(Modifiers(), newTermName("_res"), TypeTree(), c.resetAllAttrs(body)),
-            Assign(Ident(newTermName("_inst")),
-              Apply(Select(Select(Ident("scala"), newTermName("Some")), newTermName("apply")),
-                List(Ident(newTermName("_res"))))
-            )), Ident(newTermName("_res"))))
-
-
-        val applyDef = DefDef(Modifiers(), newTermName("apply"), List(),
-          List(params),
-          TypeTree(), Apply(Select(Ident(newTermName("_inst")), newTermName("getOrElse")),
-            List(Apply(Ident(newTermName("_declareInst")), params.map {
-              p => Ident(p.name)
-            }))))
-
-        val objectName = newTermName(c.fresh("TheObject"))
-
-        val obj = ModuleDef(Modifiers(), objectName, Template(objInheritance, emptyValDef, List(constructor, inst, declareInst, applyDef)))
-
-        val applyParams =  params.map { p =>
-          ValDef(Modifiers(Flag.PARAM), c.fresh(p.name), p.tpt, EmptyTree)
-        }
-
-        val partiallyApplied = Function(
-          applyParams,
-          Apply(Select(Ident(objectName), newTermName("apply")), applyParams.map {
-            p => Ident(p.name)
-          }))
-
-
-        val block = Block(List(obj), partiallyApplied)
-
-        println(show(block))
-
-        c.Expr[R](block)
-      }
-      case _ => println("no match")
-        //TODO: no param functions turn into lazy val, function references?
-        c.Expr[Any](Literal(Constant(())))
+      case f @ Function(body, params) =>
+        convertFunctionToSingletonObject(f)
+      case Block(stats, expr @ Function(_,_)) => 
+        //todo: handle stats
+        convertFunctionToSingletonObject(expr)
+      case Block(stats, expr) =>
+        c.abort(c.enclosingPosition, "No singleton function found, got block w/o ending function: " + stats + " " + expr)
+      case other if (funcs.exists { tt.tpe.typeSymbol == _ }) =>
+          val targs = tt.tpe match {
+            case TypeRef(_, _, args) => args
+            case _ => c.abort(c.enclosingPosition, "No function found, got value of type: " + other.tpe)}
+          val params = targs.slice(0, targs.length - 1).map(arg => ValDef(Modifiers(Flag.PARAM), c.fresh(arg.toString.replaceAll("[^a-zA-Z0-9]", "")), TypeTree(arg), EmptyTree))
+          val body = Apply(other, params.map {p => Ident(p.name)})
+          convertFunctionToSingletonObject(Function(params, body), Option(targs.last))
+      case nonFunction => c.abort(c.enclosingPosition, "No function found, got value of type: " + nonFunction.tpe)
     }
   }
 }
