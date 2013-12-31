95286f371ad09284226c2ff155a9fb17f45db469
Kelsey <kelseyq@gmail.com>
2013-12-30 23:51:30 -0800

    move tests to specs2

--- core/src/test/scala/detonatorTests/Test.scala
+++ core/src/test/scala/detonatorTests/Test.scala
@@ -7,7 +7,7 @@ import org.specs2.mutable._
 class PrimitivesSpec extends Specification {
 
   "Primitive singletons" should {
-    "give the same value over multiple invocations" in {
+    "give the same String value over multiple invocations" in {
         val test1: String = PrimitiveObject.sing("hello")
         Thread.sleep(1000)
         val test2: String = PrimitiveObject.sing("cruel")
@@ -15,80 +15,82 @@ class PrimitivesSpec extends Specification {
         val test3: String = PrimitiveObject.sing("world")
       (test1 must_== test2) and (test2 must_== test3)
     }
+
+    "give the same Long value over multiple invocations" in {
+      val test1: Long = PrimitiveObject.longSing("alphabet")
+      val test2: Long = PrimitiveObject.longSing("soup")
+      (test1 must_== test2) and (test2 must_== 8)
+    }
+
+    "give the same value over multiple invocations given multiple parameters" in {
+      val test1: String = PrimitiveObject.twoParamSing("mach", 5)
+      Thread.sleep(1000)
+      val test2: String = PrimitiveObject.twoParamSing("fantastic", 4)
+      (test1 must_== test2)
+    }
+  }
+
+  "Nonprimitive singletons" should {
+      "work with type parameters" in {
+        val test1 = ComplexObject.listSing(Some(10))
+        Thread.sleep(1000)
+        val test2: List[Int] = ComplexObject.listSing(None)
+        (test1 must_== test2) and (test2(1) must_== 10)
+      }
+
+    "work with a case class parameter" in {
+      val bananaParam = Banana("elite", 8)
+      val test1 = ComplexObject.caseClassParam(bananaParam)
+      Thread.sleep(1000)
+      val test2 = ComplexObject.caseClassParam(Banana("only", 1))
+      (test1 must_== test2) and (test2 must contain(bananaParam.toString))
+    }
+
+    "work with a returned case class" in {
+      val bananaParam = Banana("elite", 8)
+      val test1 = ComplexObject.caseClassReturned("flipadelphia")
+      Thread.sleep(1000)
+      val test2 = ComplexObject.caseClassReturned("chardee macdennis")
+      (test1 must_== test2) and (test2.test must_== "flipadelphia")
+    }
+
+    "take a function def" in {
+      val test1 = ComplexObject.functionDef(100)
+      val test2 = ComplexObject.functionDef(1)
+      (test1 must_== test2) and (test2 must_== "200")
+    }
+
+    "take a curried function" in {
+      val test1 = ComplexObject.curried("spca")
+      val test2 = ComplexObject.curried("mississippi")
+      (test1 must_== test2) and (test2 must_== 1004)
+    }
+
+    "take a function val" in {
+      val test1 = ComplexObject.functionVal(List(1,2,3))
+      val test2 = ComplexObject.functionVal(List(0,0,0))
+      (test1 must_== test2) and (test2(1) must_== 3)
+    }
+
+    "take a val as a function" in {
+      val test1 = ComplexObject.valAsFunction()
+      val test2 = ComplexObject.valAsFunction()
+      (test1 must_== test2)
+    }
   }
 }
 
-//object Test extends App {
-//  val result = "hello world"
-//  val test: String = PrimitiveObject.sing("hi")
-//  println(test.asInstanceOf[AnyRef].getClass.getSimpleName)
-//  println(test)
-//  println(test)
-//  Thread.sleep(1000)
-//  println(test)
-//  Thread.sleep(1000)
-//  val test2: String = PrimitiveObject.sing("hi3")
-//  println(test2.asInstanceOf[AnyRef].getClass.getSimpleName)
-//  println(test2)
-//  Thread.sleep(1000)
-//  println(test2)
-//  println(test2)
-//  def intTest: Long = PrimitiveObject.intSing("hi4")
-//  println(intTest.asInstanceOf[AnyRef].getClass.getSimpleName)
-//  println(intTest)
-//  println(intTest)
-//  println(intTest)
-//  Thread.sleep(1000)
-//  println(intTest)
-//  val doubleTest: String = PrimitiveObject.doubleSing("hi5", 7)
-//  println(doubleTest.asInstanceOf[AnyRef].getClass.getSimpleName)
-//  println(doubleTest)
-//  println(doubleTest)
-//  println(doubleTest)
-//  Thread.sleep(1000)
-//  println(doubleTest)
-//  val listTest: List[Int] = PrimitiveObject.listSing(Some(365))
-//  println(listTest.asInstanceOf[AnyRef].getClass.getSimpleName)
-//  println(listTest)
-//  println(listTest)
-//
-//  val ccTest = ComplexObject.caseClassParam(Banana("hi", 5))
-//  println(ccTest.asInstanceOf[AnyRef].getClass.getSimpleName)
-//  println(ccTest)
-//  println(ccTest)
-//
-//  val caseClassReturned = ComplexObject.caseClassReturned("apple")
-//  println(caseClassReturned.asInstanceOf[AnyRef].getClass.getSimpleName)
-//  println(caseClassReturned)
-//  println(caseClassReturned)
-//
-//  val functionTest = ComplexObject.functionDef(100)
-//  println(functionTest.asInstanceOf[AnyRef].getClass.getSimpleName)
-//  println(functionTest)
-//  println(functionTest)
-//
-//  val valAsFunctionTest = ComplexObject.functionVal(List(1,2,3))
-//  println(valAsFunctionTest.asInstanceOf[AnyRef].getClass.getSimpleName)
-//  println(valAsFunctionTest)
-//  println(valAsFunctionTest)
-//
-//  val curriedTest = ComplexObject.curried("this is a string")
-//  println(curriedTest.asInstanceOf[AnyRef].getClass.getSimpleName)
-//  println(curriedTest)
-//  println(curriedTest)
-//}
-
 object PrimitiveObject {
   val sing = Macros.singleton{ (test: String) => { val time = System.currentTimeMillis().toString; time } }
-  lazy val intSing = Macros.singleton { (test: String) => { val time = System.currentTimeMillis(); println(test + " " + time); time } }
-  val doubleSing = Macros.singleton{ (test1: String, test2: Int) => { val time = System.currentTimeMillis().toString; println(test1 + " " + time + " " + test2); time } }
-  val listSing = Macros.singleton { (testParam: Option[Int]) => println("listTest " + System.currentTimeMillis()); testParam.map(v => List(v, v, v)).getOrElse(List(0,0,0))}
+  lazy val longSing = Macros.singleton { (test: String) => test.length }
+  val twoParamSing = Macros.singleton{ (test1: String, test2: Int) => System.currentTimeMillis().toString + " " + test1 + " " + test2 }
 }
 
 object ComplexObject {
+  val listSing = Macros.singleton { (testParam: Option[Int]) => testParam.map(v => List(v + System.currentTimeMillis().toInt, v, v)).getOrElse(List(0,0,0))}
 
-  val caseClassParam = Macros.singleton{ (ccParam: Banana) => { println(System.currentTimeMillis().toString); ccParam.toString } }
-  val caseClassReturned = Macros.singleton{ (test: String) => { println(System.currentTimeMillis().toString); Banana(test, 200) } }
+  val caseClassParam = Macros.singleton{ (ccParam: Banana) => { ccParam.toString + " " + System.currentTimeMillis().toString } }
+  val caseClassReturned = Macros.singleton{ (test: String) => { Banana(test, System.currentTimeMillis().toInt) } }
 
   def newFunction(something: Int): String = (something * 2).toString
   val functionDef = Macros.singleton {newFunction _}
@@ -96,14 +98,14 @@ object ComplexObject {
   def curryable(oneParam: Banana, twoParam: String) = oneParam.test2 + twoParam.length
   val curried = Macros.singleton(curryable(Banana("hola", 1000), _: String))
 
-  val valFunction = (something: List[Int]) => (something.map(_ * 3)).toString + System.currentTimeMillis()
+  val valFunction = (something: List[Int]) => System.currentTimeMillis().toInt :: something.map(_ * 3)
   val functionVal = Macros.singleton(valFunction)
 
-//  //TODO: should fail
+//  //TODO: should translate to lazy val
 //  val wrappedString = "hello"
 //  val stringAsFunction = Macros.singleton(wrappedString)
 //
-//  //TODO: should fail
+//  //TODO: should translate to lazy val
 //  val nonPrimitive = Banana("yo", 10)
 //  val nonFunction = Macros.singleton(nonPrimitive)
 
